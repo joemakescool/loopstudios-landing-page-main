01407a4cc1845b1087b2ed05c379fcf7efe8d8d8
joebarker <joebarker@mail.weber.edu>
2021-03-05 16:24:40 -0700

    weekend
    got it all build. now refinement

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="navigation navigation--footer">
+    <li>About</li>
+    <li>Careers</li>
+    <li>Events</li>
+    <li>Products</li>
+    <li>Support</li>
+  </ul>
+</template>
+
+<script>
+export default {
+name: "Navigation"
+}
+</script>
+
+<style scoped>
+
+</style>
